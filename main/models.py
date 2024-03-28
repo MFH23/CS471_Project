ed552e7a9e6b52fa6eb043de01cdc82a0024fb4a
MFH23 <=>
2024-03-28 06:49:27 +0300

    Model for contact

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=10)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.email
--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render ,HttpResponse
+from .models import Contact
 
 
 # Create your views here.
@@ -10,6 +11,29 @@ def about(request):
     pass
 
 def contact(request):
+    if request.method == "POST":
+        first_name = request.POST.get('first_name', '')
+        last_name = request.POST.get('last_name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        message = request.POST.get('message', '')
+
+        
+        # basic validation
+        if first_name and last_name and email and message:
+            contact = Contact(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                phone=phone,
+                message=message
+            )
+            contact.save()
+            return HttpResponse("<h1>Thanks</h1>")
+        else:
+            return HttpResponse("<h1>Error: Please fill in all required fields</h1>")
+
+
     return render(request, 'main/contact.html')
 
 def feedback(request):
