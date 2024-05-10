9bac16eb99c6ff80af1b7e2968fd855c0da52649
MFH23 <=>
2024-05-10 05:49:44 +0300

    Signup & login system

--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render ,HttpResponse
+from django.shortcuts import render ,HttpResponse,redirect
 from .models import Contact
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignupForm, LoginForm
 
 
 # Create your views here.
@@ -44,3 +46,35 @@ def services(request):
 
 def appointment(request):
     pass
+
+
+# signup page
+def user_signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
+    return render(request, 'main/signup.html', {'form': form})
+
+# login page
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)    
+                return render(request, 'main/home.html')
+    else:
+        form = LoginForm()
+    return render(request, 'main/login.html', {'form': form})
+
+# logout page
+def user_logout(request):
+    logout(request)
+    return render(request, 'main/home.html')
